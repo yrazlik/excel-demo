3979f4188956f2a8b3534e0cc800e40dd44db0cf
Yasin Razlık <yasin.razlik@obss.com.tr>
2024-09-24 21:23:42 +0300

    projects created

---
+++ excel-demo-be/src/main/java/com/example/excel/excel_demo_be/ExcelDemoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.excel.excel_demo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDemoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDemoBeApplication.class, args);
+	}
+
+}
---
+++ excel-demo-be/src/test/java/com/example/excel/excel_demo_be/ExcelDemoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel.excel_demo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDemoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
